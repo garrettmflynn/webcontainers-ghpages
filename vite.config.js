08831638b2af85f14e5ba9bb4f3a372c20f66676
Garrett <gflynn@usc.edu>
2023-03-01 19:00:39 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: "./"
+    base: "./",
+//     server: {
+//         headers: {
+//             'Cross-Origin-Embedder-Policy': 'require-corp',
+//             'Cross-Origin-Opener-Policy': 'same-origin',
+//         },
+//     },
 });
